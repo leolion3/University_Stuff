4c89c2f45ba561f530d05b974c9759c01835f8e7
Leonard Haddad <s_xsipo6@uni-bremen.de>
2019-09-26 15:22:19 +0200

    updated

--- Etc in der Freizeit/Python/Chatroom/Chatroom.py
+++ Etc in der Freizeit/Python/Chatroom/Chatroom.py
@@ -10,10 +10,11 @@ PORT = 5050
 clients = []
 client_names = []
 username = ''
+client_addresses = []
 
 
-def client_handler(s,client_name):
-    global clients,client_names
+def client_handler(s,client_name,client_address):
+    global clients,client_names,client_addresses
     try:
         while True:
             data = s.recv(4096)
@@ -29,17 +30,33 @@ def client_handler(s,client_name):
                     print(message)
                     for a in clients:
                         if a!=s:
-                            a.send(bytes("\n"+message,"UTF-8"))
+                            a.send(bytes(message,"UTF-8"))
     except:
         client_names.remove(client_name)
         clients.remove(s)
         for a in clients:
-            a.send(bytes("\n#>SYSTEM: "+client_name + " has left the chatroom!\n","UTF-8"))
+            a.send(bytes("#>SYSTEM: "+client_name + " has left the chatroom!","UTF-8"))
+        print("#>SYSTEM: "+client_name + " has left the chatroom!")
+        client_addresses.remove(client_address)
         s.close()
 
 
+def view_users():
+    global client_addresses
+    while True:
+        server_input = input()
+        if len(server_input):
+            if "/users" in server_input:
+                print("\n=====Currently Connected=====")
+                for a in client_addresses:
+                    print(str(a[0])+":"+str(a[1]))
+                print("=============================")
+            elif server_input.lower() == 'cls' or server_input.lower() == 'clear':
+                os.system('cls' if os.name == 'nt' else 'clear')
+
+
 def start_chatroom():
-    global ADDRESS,PORT,clients,client_names
+    global ADDRESS,PORT,clients,client_names,client_addresses
     print("Please select address to host on!")
     address = input("#>")
     if len(address):
@@ -54,17 +71,21 @@ def start_chatroom():
         s.listen(True)
         os.system('cls' if os.name == 'nt' else 'clear')
         print("Listening on " + ADDRESS +":"+ str(PORT))
+        print('To view all connected users, type "/users"')
+        p = threading.Thread(target = view_users)
+        p.start()
         while True:
             client_socket, client_address = s.accept()
-            print("Client " + str(client_address[0])+":"+str(client_address[1]) + " connected!")
             client_name = client_socket.recv(1024).decode()
             client_names.append(client_name)
-            t = threading.Thread(target = client_handler, args = (client_socket,client_name))
+            t = threading.Thread(target = client_handler, args = (client_socket,client_name,client_address))
             t.setDaemon(True)
             t.start()
             for a in clients:
-                a.send(bytes("\n#>SYSTEM: "+client_name + " has joined the chatroom!\n","UTF-8"))
+                a.send(bytes("#>SYSTEM: "+client_name + " has joined the chatroom!","UTF-8"))
             clients.append(client_socket)
+            print("#>SYSTEM: "+client_name + " has joined the chatroom!")
+            client_addresses.append(client_address)
     except:
         print("Error occurred! Terminating...")
         time.sleep(3)
@@ -89,6 +110,7 @@ def send_messages(s):
                 sendme = "<"+username +">: " + message
                 s.send(bytes(sendme,"UTF-8"))
             continue
+
             
 def join():
     global ADDRESS,PORT,username
@@ -121,7 +143,6 @@ def join():
         print("Error occurred! Exiting...")
         time.sleep(3)
         sys.exit(0)
-
 
 
 def select_type():
