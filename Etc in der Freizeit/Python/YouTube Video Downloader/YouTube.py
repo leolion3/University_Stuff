9a0620925c160be7d066a13dc33ebfa9cd07ac77
Leonard Haddad <s_xsipo6@uni-bremen.de>
2020-01-20 12:35:11 +0100

    .

--- Etc in der Freizeit/Python/YouTube Video Downloader/YouTube.py
+++ Etc in der Freizeit/Python/YouTube Video Downloader/YouTube.py
@@ -6,17 +6,45 @@
 # you can install ffmpeg using pip install ffmpeg
 # if that doesnt work, copy the exe's to the install location of youtube-dl.exe
 import subprocess
+import threading
+import pyperclip
+import time
+import sys
+import os
+sys.path.append(os.path.abspath("SO_site-packages"))
+
+
+def download(command):
+    subprocess.run(command, shell=True)
 
 
 def start():
+    print("Copy link to clipboard to start downloading!")
+    inp = ""
     while True:
-        print("Please insert link!")
-        inp = input("#>")
-        try:
-            command = "youtube-dl -x --audio-format mp3 " + inp
-            subprocess.run(command, shell=True)
-        except:
-            print("Error or wrong link!")
+        tmp = pyperclip.paste()
+        if inp!=tmp:
+            inp = tmp
+            if inp == "exit":
+                exit(0)
+            elif "&list" in inp:
+                try:
+                    link, pl = inp.split("list=")
+                    if "?" in pl:
+                        playlist, rest = pl.split("?")
+                    else:
+                        playlist = pl
+                    command = 'youtube-dl -i ' + playlist + ' -x --audio-format mp3'
+                    download(command)
+                except:
+                    print("Error!")
+            else:
+                try:
+                    command = 'youtube-dl -x --audio-format mp3 ' + inp
+                    t = threading.Thread(target=download, args=(command,))
+                    t.start()
+                except:
+                    print("Error or wrong link!")
 
 
 if __name__ == "__main__":
